de3b5faf85f470bd4cd76a6ffd1085d85583de29
WILSON220374 <wesanchezh@gmail.com>
2026-05-07 10:06:47 -0500

    Add new page for Informe de interventoría

--- app.py
+++ app.py
@@ -308,6 +308,7 @@ else:
         st.Page("views/25_seguimiento_fisico.py", title="25. Seguimiento físico", icon="📉"),
         st.Page("views/27_acta_reanudacion.py", title="27. ACTA DE REANUDACIÓN", icon="▶️"),
         st.Page("views/28_modificacion_prorroga.py", title="28. Modificación y prórroga", icon="📝"),
+        st.Page("views/29_informe_interventoria.py", title="29. Informe de interventoría", icon="📄"),
         
     ]
 
